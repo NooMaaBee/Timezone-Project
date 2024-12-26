07a4bafbd8a42c1cc99da11045c40e7c091b0bab
NooMaaBee <latteliten@hotmail.com>
2024-12-26 21:49:39 +0100

    removed link

--- src/index.js
+++ src/index.js
@@ -61,7 +61,6 @@ function displayCityTime(cityTimeZone, cityName) {
               "h:mm:ss"
             )}<small>${cityTime.format("A")}</small></div>
           </div>`;
-  <a href="/">All cities</a>;
 }
 
 updateTime();
